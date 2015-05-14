df7b7a6fa737dd438792694ad2f96d69170bc2db
Daniel Dropik <ddropik@wisc.edu>
2015-05-14 15:54:56 -0500

    Building standard sass file structure, update style.css
    style.css: Includes the template data for the child theme since it's needed for a proper child theme.
    
    Includes a starting template for a custom gulpfile.js.

---
+++ gulpfile.js
@@ -0,0 +1,109 @@
+// Load plugins
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    autoprefixer = require('gulp-autoprefixer'),
+    minifycss = require('gulp-minify-css'),
+    jshint = require('gulp-jshint'),
+    uglify = require('gulp-uglify'),
+    imagemin = require('gulp-imagemin'),
+    rename = require('gulp-rename'),
+    clean = require('gulp-clean'),
+    concat = require('gulp-concat'),
+    notify = require('gulp-notify'),
+    cache = require('gulp-cache'),
+    newer = require('gulp-newer'),
+    livereload = require('gulp-livereload'),
+    lr = require('tiny-lr'),
+    server = lr();
+
+// Styles
+gulp.task('styles', function() {
+  return gulp.src('sass/master.scss')
+    .pipe(sass({ style: 'expanded', errLogToConsole: true }))
+    .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
+    .pipe(gulp.dest('dist/styles/unminified'))
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(minifycss())
+    .pipe(livereload(server))
+    .pipe(gulp.dest('dist/styles/minified'))
+    .pipe(notify({ message: 'Styles task complete' }));
+});
+
+
+// Scripts
+gulp.task('scripts', function() {
+  return gulp.src(['./js/jquery-2.1.3.js','./js/velocity.js','./js/velocity.ui.js','./js/CALSboilerplate_underscores_custom.js','./js/parallax.js','./js/tabs.js','./js/menu.js'])
+    .pipe(concat('main.js'))
+    .pipe(gulp.dest('dist/scripts'))
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(uglify())
+    .pipe(livereload(server))
+    .pipe(gulp.dest('dist/scripts'))
+    .pipe(notify({ message: 'Scripts task complete' }));
+});
+
+
+// Script Debug
+gulp.task('debug', function() {
+  return gulp.src(['./js/parallax.js'])
+    .pipe(jshint('.jshintrc'))
+    .pipe(jshint.reporter('default'))
+    .pipe(notify({ message: 'Debug Scripts task complete' }));
+});
+
+//
+gulp.task('hello', function(){
+    console.log('hello world');
+});
+
+// Images
+gulp.task('images', function() {
+  return gulp.src('img/**/*')
+    .pipe(cache(imagemin({ svgoPlugins: [{removeViewBox: false}], progressive: true })))
+    .pipe(livereload(server))
+    .pipe(gulp.dest('dist/images'))
+    .pipe(notify({ message: 'Images task complete' }));
+});
+
+
+
+// Clean
+gulp.task('clean', function() {
+  return gulp.src(['dist/styles', 'dist/scripts', 'dist/images'], {read: false})
+    .pipe(clean());
+});
+
+//clear cache
+gulp.task('clear', function (done) {
+    return cache.clearAll(done);
+});
+
+// Default task
+gulp.task('default', ['clean'], function() {
+    gulp.start('styles', 'scripts', 'images');
+});
+
+// Watch
+gulp.task('watch', function() {
+
+  // Listen on port 35729
+  server.listen(35729, function (err) {
+    if (err) {
+      return console.log(err)
+    };
+
+    // Watch .scss files
+    gulp.watch('sass/**/*.scss', ['styles']);
+
+    // Watch .js files
+    gulp.watch('js/**/*.js', ['scripts']);
+
+    // Watch image files
+    gulp.watch('img/**/*', ['images']);
+
+
+
+
+  });
+
+});
