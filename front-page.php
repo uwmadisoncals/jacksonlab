b55f8e0645b292ca8a380b07471c43e09ea040b9
Daniel Dropik <ddropik@wisc.edu>
2015-06-15 10:36:07 -0500

    Custom WP_Query & Args on front-page.php

--- front-page.php
+++ front-page.php
@@ -15,8 +15,9 @@ get_header(); ?>
 
 <?php 
 $args = array(
-"posts_per_page" = > "-1" /*,
-"cat"=>"" */
+"posts_per_page" => "-1",
+//"cat"=>"25"
+"cat"=>"666"
 );
 
 $frontpageQuery = new WP_Query($args) ; 
@@ -32,14 +33,20 @@ $frontpageQuery = new WP_Query($args) ;
 		
 			<div id="content" role="main">
 
-				<?php while ( have_posts() ) : the_post(); ?>
+				<?php if($frontpageQuery->have_posts()) : ?>
 
-					<?php get_template_part( 'content', 'page' ); ?>
+					<?php while ( $frontpageQuery->have_posts() ) : $frontpageQuery->the_post(); ?>
 
-					<?php comments_template( '', true ); ?>
+						<?php get_template_part( 'content', 'front_page' ); ?>
 
-				<?php endwhile; // end of the loop. ?>
-				
+						<?php comments_template( '', true ); ?>
+
+					<?php endwhile; // end of the loop. ?>
+
+				<?php else: ?>
+					<p>sorry no posts found</p>
+
+				<?php endif; ?>
 			</div><!-- #content -->
 			<?php// get_sidebar(); ?>
 			<div class="clear"></div>
