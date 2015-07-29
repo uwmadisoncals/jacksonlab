f27143d38b57d3a5fc329e01e58a82d8d825087f
Daniel Dropik <ddropik@wisc.edu>
2015-07-29 15:45:26 -0500

    Revert "comments out custom_field_excerpt in functions.php"
    This reverts commit 3579fc1fde742f123343410ed40e09d233ca355b.

--- functions.php
+++ functions.php
@@ -106,7 +106,7 @@ function my_admin_notice(){
 add_action('admin_notices', 'my_admin_notice');
 */
 
-/*
+
 function custom_field_excerpt() {
 
   global $post;
@@ -127,4 +127,3 @@ function custom_field_excerpt() {
   }
   return apply_filters('the_excerpt', $text);
 }
-*/
