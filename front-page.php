2cfaaa1298a2aac1712a50d1ec542fe84c73c041
Daniel Dropik <ddropik@wisc.edu>
2015-08-14 03:32:07 -0500

    adds styles to boxes when no featured img set

--- front-page.php
+++ front-page.php
@@ -176,9 +176,22 @@ get_header(); ?>
 											<?php else: ?>
 	    
 										
-											<h2><a href="<?php the_permalink(); ?>" ><?php the_title(); ?></a></h2>
-											
+
+
+											<div class="imgWrap">
+
+		    								<div class="pane"></div> 
+		    								
+		    									<div class="imgWrapContainer">
+		    										<div class="imgWrapPic"></div>
+		    									</div>
+		    									<h2><?php the_title(); ?></h2>
+		    								
 												<p><?php the_excerpt(); ?></p>
+												
+												<a href="<?php the_permalink(); ?>" class="coverLink"><?php the_title(); ?></a>
+		    								
+		    								</div><!-- END .imgWrap -->
 											
 											
 											<?php endif; ?>
